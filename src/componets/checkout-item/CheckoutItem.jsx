9c47d359e7f467e927da4ca561559871c35fb895
Settarov <settarov.ali@gmail.com>
2021-05-11 00:25:07 +0200

    Added remove and add items functionality on Checkout page

--- src/componets/checkout-item/CheckoutItem.jsx
+++ src/componets/checkout-item/CheckoutItem.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
 import { connect } from "react-redux";
-import { clearItemFromCart } from "../../redux/cart/cartActions";
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem,
+} from "../../redux/cart/cartActions";
 import ClearItem from "../clear-item/ClearItem";
+
+import RemoveCircleOutlineIcon from "@material-ui/icons/RemoveCircleOutline";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 
 import "./CheckoutItem.scss";
 
-const CheckoutItem = ({ cartItem, clearItem }) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
   const { imageUrl, name, quantity, price } = cartItem;
   return (
     <div className="checkout-item">
@@ -14,7 +21,21 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
         <img src={imageUrl} alt="Item" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <div className="quantity">
+        <RemoveCircleOutlineIcon
+          onClick={() => removeItem(cartItem)}
+          className="btn"
+          fontSize="small"
+          color="error"
+        />
+        <div className="quantity-inner">{quantity}</div>
+        <AddCircleOutlineIcon
+          className="btn"
+          fontSize="small"
+          color="primary"
+          onClick={() => addItem(cartItem)}
+        />
+      </div>
       <span className="price">{price}</span>
       <div className="remove-button" onClick={() => clearItem(cartItem)}>
         <ClearItem size="default" />
@@ -25,6 +46,8 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
 
 const mapDispatchToProps = (dispatch) => ({
   clearItem: (item) => dispatch(clearItemFromCart(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item)),
 });
 
 export default connect(null, mapDispatchToProps)(CheckoutItem);
