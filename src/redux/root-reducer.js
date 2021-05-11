806a99ce1d082155f00fadea853c6591f2d2c34c
Settarov <settarov.ali@gmail.com>
2021-05-11 19:37:47 +0200

    Implemented localStorage using redux-persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,19 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import userReducer from "./user/userReducer";
 import cartReducer from "./cart/cartReducer";
 
-const reducer = combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
 
-export default reducer;
+export default persistReducer(persistConfig, rootReducer);
