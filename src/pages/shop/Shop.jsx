b04193ba57e041575460ae56abb54b772b4cfca8
Settarov Ali <settarov.ali@gmail.com>
2021-05-20 04:54:17 +0200

    Changed Observer pattern

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -26,17 +26,29 @@ class ShopPage extends Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection("collections");
 
-    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
-      async (snapshot) => {
-        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-        updateCollections(collectionsMap);
-        this.setState({ loading: false });
-      }
-    );
+    // this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
+    //   async (snapshot) => {
+    //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    //     updateCollections(collectionsMap);
+    //     this.setState({ loading: false });
+    //   }
+    // );
+
+    // fetch(
+    //   `https://firestore.googleapis.com/v1/projects/crwn-db-15f3f/databases/(default)/documents/collections`
+    // )
+    //   .then((response) => response.json())
+    //   .then((collections) => console.log(collections));
+
+    collectionRef.get().then((snapshot) => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+      this.setState({ loading: false });
+    });
   }
 
   componentWillUnmount() {
-    this.unsubscribeFromSnapshot();
+    // this.unsubscribeFromSnapshot();
   }
 
   render() {
